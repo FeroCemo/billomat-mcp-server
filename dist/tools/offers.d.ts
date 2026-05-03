f57bfebfebb01748686cd75b8e38405bd276b5aa
FeroCemo <juicness.312@gmail.com>
2026-05-03 17:12:14 +0200

    Add dist/tools/offers.d.ts

---
+++ dist/tools/offers.d.ts
@@ -0,0 +1,4 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { BillomatClient } from "../services/client.js";
+export declare function registerOfferTools(server: McpServer, api: BillomatClient): void;
+//# sourceMappingURL=offers.d.ts.map
