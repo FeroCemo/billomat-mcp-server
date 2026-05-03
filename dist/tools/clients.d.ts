30a2219e89a057527974a8b56cc3baacd053c10f
FeroCemo <juicness.312@gmail.com>
2026-05-03 17:04:38 +0200

    initial: billomat mcp server

---
+++ dist/tools/clients.d.ts
@@ -0,0 +1,4 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { BillomatClient } from "../services/client.js";
+export declare function registerClientTools(server: McpServer, api: BillomatClient): void;
+//# sourceMappingURL=clients.d.ts.map
